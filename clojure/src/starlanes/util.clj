596e76cf07d5cbde3fe88293a86077cf30a21b2e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-08 20:04:30 -0500

    Added more utility functions.

--- clojure/src/starlanes/util.clj
+++ clojure/src/starlanes/util.clj
@@ -1,5 +1,20 @@
-(ns starlanes.util)
+(ns starlanes.util
+  (:require [clojure.string :as string]
+            [clojure.set :refer [intersection]]))
 
+(def fake-game-data
+  {:star-map
+    {:a1 ".", :a2 ".", :a3 ".", :a4 ".", :a5 ".", :b1 ".", :b2 ".",
+     :b3 ".", :b4 ".", :b5 ".", :c1 ".", :c2 ".", :c3 "*", :c4 ".",
+     :c5 ".", :d1 ".", :d2 ".", :d3 ".", :d4 ".", :d5 ".", :e1 ".",
+     :e2 ".", :e3 ".", :e4 ".", :e5 "."},
+   :total-moves 0,
+   :players [{:stock nil, :name "Alice", :cash 0.0}],
+   :player-order [0],
+   :move 0,
+   :companies [],
+   :share-value {},
+   :rand nil})
 
 (defn display [data]
   (.print (System/out) data))
@@ -27,3 +42,56 @@
 
 (defn rand-game [game-data]
   (rand-float (:rand game-data)))
+
+(defn coord-open? [coord empty-string]
+  (cond
+    (= (last coord) empty-string) true
+    :else false))
+
+(defn keyword->xy
+  "Given a coordinate in keyword-form, return the x and y components as a
+   vector."
+  [keyword-coord]
+  (let [string-name (name keyword-coord)
+        string-len (count string-name)
+        x-coord (take 1 string-name)
+        y-coord (take-last (- string-len 1) string-name)]
+    [(apply str x-coord)
+     (apply str y-coord)]))
+
+(defn get-friendly-coord
+  "Given a coord (a keyword such as :a23), return a format that is easier for
+   a player to read (by row, then columns)."
+  [keyword-coord]
+  (let [[x-coord y-coord] (keyword->xy keyword-coord)]
+    (string/join [y-coord x-coord])))
+
+(defn is-item? [coord-data expeted-item-char]
+  (cond
+    (= (last coord-data) expeted-item-char) true
+    :else false))
+
+(defn filter-item
+  "This function is intended to be used as a parameter passed to a map
+   function."
+  [coord-data expeted-item-char]
+  (cond
+    (is-item? coord-data expeted-item-char) coord-data
+    :else nil))
+
+(defn filter-allowed [all legal]
+  (intersection (set all) (set legal)))
+
+(defn get-valid-coord-range
+  "Given the integer representation of a coordinate component (e.g., either a
+   value for x or a value for y, not both), as well as the minimum and maximum
+   possible values for the axis of the given component (e.g., x- or y-axis),
+   XXX
+   "
+  [coords min max]
+  (filter-allowed
+    coords
+    (range min (+ max 1))))
+
+
+
