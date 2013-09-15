113a0985c279b1c82cc16d431ba2717a5f05de35
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-15 16:39:51 -0500

    Moved finance functions from game to finance.

--- clojure/src/starlanes/finance.clj
+++ clojure/src/starlanes/finance.clj
@@ -10,3 +10,22 @@
   (util/input const/continue-prompt)
   nil)
 
+(defn compute-stock-value [{stock :stock value :value}]
+  (* stock value))
+
+(defn compute-stocks-value [stocks-data]
+  (reduce + (map compute-stock-value stocks-data)))
+
+(defn compute-value
+  "The assets parameter is a mapwhich has the following structure:
+    {:cash <float> :stock <integer> :value <float>}
+  where :value is stock price of the associated stock.
+  "
+  ([assets]
+    (apply compute-value assets))
+  ([cash stocks-data]
+    (+ cash (compute-stocks-value stocks-data))))
+
+(defn display-value [value]
+  )
+
