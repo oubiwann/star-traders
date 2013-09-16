68dbe2fd9a29fe09aca48f8c85ef520e2bd40b2e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-15 22:09:49 -0500

    Added namespaces.

---
+++ clojure/test/starlanes/game/command_test.clj
@@ -0,0 +1,4 @@
+(ns starlanes.game.base-test
+  (:require [clojure.test :refer :all]
+            [starlanes.game.command :as game]
+            [starlanes.util :as util]))
---
+++ clojure/test/starlanes/game/movement_test.clj
@@ -0,0 +1,4 @@
+(ns starlanes.game.base-test
+  (:require [clojure.test :refer :all]
+            [starlanes.game.movement :as game]
+            [starlanes.util :as util]))
